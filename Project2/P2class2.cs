4273bdce4b86e1b66b7eb8b2a1f0f8d7b849eb99
rsappidi <rsappidi@PC05WWL8>
2019-02-21 23:39:47 -0600

    Commit to Project2

---
+++ Project2/P2class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project2
+{
+    [TestClass]
+    public class P2class2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
